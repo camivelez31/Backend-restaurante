4db21f211682e90290a144db076b6fd5b0e2e4d9
Maria Camila Vélez Mazo <camilavelezdisenadoragmail.com>
2026-05-30 18:44:34 -0500

    fix: agregar origen cors para live server

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ origins = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8080",
     "http://localhost:8080",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 app.add_middleware(
